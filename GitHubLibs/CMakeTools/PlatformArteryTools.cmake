809fd24cc7b4ebc4981e5c2f093225806e4da2c8
kozulev@mzta.ru <Kozulev@mzta.ru>
2023-11-15 11:03:43 +0300

    dell newlib
    удалены заглушки для работы с библиотекой newlib, данное решение
    позволяет стандартизированно работать с динамической памятью

--- GitHubLibs/CMakeTools/PlatformArteryTools.cmake
+++ GitHubLibs/CMakeTools/PlatformArteryTools.cmake
@@ -8,14 +8,17 @@ set(MCU_CFlags "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 ${CPU
 
 set(PlatformSrc "${PlatformSrc}" "${StartupSrc}" "${SystemSrc}")
 
+
+
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${MCU_CFlags}")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${MCU_CFlags} -T${LinkerScript}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${MCU_CFlags} -T${LinkerScript} -lc -lrdimon")
 message("change CMAKE_C_FLAGS::${CMAKE_C_FLAGS}")
 message("change CMAKE_EXE_LINKER_FLAGS::${CMAKE_EXE_LINKER_FLAGS}")
 
 file(GLOB_RECURSE ArteryDrivers "${CMAKE_SOURCE_DIR}/GitHubLibs/ArteryTec/AT_437_435/libraries/drivers/src/*.c")
 add_library(DriversAt_437_435 "${ArteryDrivers}")
 
+set_target_properties(DriversAt_437_435 PROPERTIES LINK_DEPENDS ${LinkerScript})
 set(ArteryCMSIS "${CMAKE_SOURCE_DIR}/GitHubLibs/ArteryTec/AT_437_435/libraries/cmsis/cm4/core_support")
 set(ArteryDeviseHead "${CMAKE_SOURCE_DIR}/GitHubLibs/ArteryTec/AT_437_435/libraries/cmsis/cm4/device_support")
 set(ArteryDriversHead "${CMAKE_SOURCE_DIR}/GitHubLibs/ArteryTec/AT_437_435/libraries/drivers/inc")
